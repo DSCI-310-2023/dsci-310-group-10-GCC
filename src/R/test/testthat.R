d328282ab5ee311a05b808a7caa0808ad589a076
jh22d <jh0220x@gmail.com>
2023-03-22 23:50:15 -0700

    let testthat.R run all tests at the same time

---
+++ src/R/test/testthat.R
@@ -0,0 +1,8 @@
+library(tidyverse)
+library(here)
+library(testthat)
+
+source(here("testthat/test-convert_to_num.R"))
+source(here("testthat/test-df_load.R"))
+source(here("testthat/test-fire_training_fn.R"))
+source(here("testthat/test-plot_fn.R"))
