e6ea4244e6c810002990f099fe47e8e9d640c853
Jan <91697947+miniatureseal@users.noreply.github.com>
2023-03-23 21:36:00 -0700

    Revert "third script"

--- src/03_data_visualization.R
+++ src/03_data_visualization.R
@@ -4,16 +4,8 @@ library(GGally)
 library(tidymodels)
 library(here)
 library(testthat)
-library(ggplot2)
-#library(plotly)
 
 options(repr.matrix.max.rows = 6)
-
-# source("src/01_data_loading.R")
-# source("src/02_data_processing.R")
-source(here("src/R/plot_fn.R"))
-
-setwd("results")
 
 # resize so we can see the plots
 options(repr.plot.width = 20, repr.plot.height = 20)
@@ -21,22 +13,15 @@ options(repr.plot.width = 20, repr.plot.height = 20)
 # so we can see the general picture
 # we add the color argument so we can compare the relationship
 # between the 2 columns and the classes column
-
-fire_train <- read.csv(here("results/fire_train.csv"))
-fire_test <- read.csv(here("results/fire_test.csv"))
-
-fire_train_plot <- fire_train %>%
+fire_train %>%
     ggpairs(aes(color = Classes))
 
-# Saving graph
-# ggsave("fire_train.png", plot = fire_train_plot, device = "png")
-
-scatter_plot <- plot_scatter_graph(data=fire_train, plot_width=8.9, plot_height=6, x_axis_data=ISI, 
-                               y_axis_data=BUI, x_axis_label="Initial Spread Index", y_axis_label="presence of fire", 
-                               text_size=20, color=Classes, color_label="presence of fire")
-              
-# Saving graph
-ggsave("scatter_plot.png", plot = scatter_plot, device = "png")
+plot_scatter_graph(data = fire_train, plot_width = 8.9,
+                    plot_height = 6, x_axis_data = ISI,
+                    y_axis_data = BUI, x_axis_label = "Initial Spread Index",
+                    y_axis_label = "presence of fire",
+                    text_size = 20,
+                    color = Classes, color_label = "presence of fire")
 
 set.seed(123)
 # making the 10-fold cross validation,
@@ -73,14 +58,11 @@ knn_results <- workflow() %>%
 accuracies <- knn_results %>%
   filter(.metric == "accuracy")
 
-# Save the csv
-write_csv(accuracies, "accuracies.csv")
+accuracies
 
-line_plot <- plot_line_graph(data=accuracies, plot_width=10, plot_height=10, x_axis_data=neighbors, 
-                            y_axis_data=mean, x_axis_label="Neighbors", y_axis_label="Accuracy Estimate")
-
-# Save the graph
-ggsave("line_plot.png", plot = line_plot, device = "png")
+plot_line_graph(data = accuracies, plot_width = 10, plot_height = 10,
+                x_axis_data = neighbors, y_axis_data = mean,
+                x_axis_label = "Neighbors", y_axis_label = "Accuracy Estimate")
 
 # we don't need to make a new recipe,
 # since we can reuse the recipe from the last step.
@@ -100,18 +82,12 @@ knn_fit <- workflow() %>%
 # we use the knn_fit to predict the class in fire_test,
 # and then we add that .pred_class column to the testing set.
 fire_test_predictions <- predict(knn_fit, fire_test) %>%
-  bind_cols(fire_test) %>%
-  mutate(Classes = as.factor(Classes))
+  bind_cols(fire_test)
 
-# Save the csv
-write_csv(fire_test_predictions, "fire_test_predictions.csv")
+fire_test_predictions
+
 # getting the confusion matrix,
 # setting the "real" column to Classes and the predicted column .pred_class
 
-# fire_test_predictions_table <- fire_test_predictions %>%
-#   conf_mat(truth = Classes, estimate = .pred_class)
-
-# fire_test_predictions_csv <- as.data.frame.matrix(fire_test_predictions_table)
-
-# # Save the table
-# write_csv(fire_test_predictions_csv, "fire_test_predictions_table.csv")
+fire_test_predictions %>%
+  conf_mat(truth = Classes, estimate = .pred_class)
