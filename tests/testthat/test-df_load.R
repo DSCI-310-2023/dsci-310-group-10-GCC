596e114de831d877149c6106842a4219ea1a10c5
jh22d <jh0220x@gmail.com>
2023-04-07 17:35:05 -0700

    replace “” with R readable signs

--- tests/testthat/test-df_load.R
+++ tests/testthat/test-df_load.R
@@ -17,7 +17,7 @@ df2_expect <- data.frame(
   sepal_width = c(3.5, 3.5, 3.5, 3, 3.2),
   petal_length = c(1.4, 1.4, 1.4, 1.4, 1.3),
   petal_width = c(0.2, 0.2, 0.2, 0.2, 0.2),
-  species = c(“setosa”, “setosa”, “setosa”, “setosa”, “setosa”)
+  species = c("setosa", "setosa", "setosa", "setosa", "setosa")
 ) %>%
   mutate(species = as_factor(species))
 
